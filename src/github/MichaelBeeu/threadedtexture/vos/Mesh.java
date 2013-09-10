f67a8bf74f17fa4a392e014b4f4104e19a2a31d6
MichaelBeeu <michael.beeu@gmail.com>
2013-09-10 05:44:11 -0500

    Basic texturing in shader/mesh.

--- src/github/MichaelBeeu/threadedtexture/vos/Mesh.java
+++ src/github/MichaelBeeu/threadedtexture/vos/Mesh.java
@@ -13,6 +13,7 @@ import android.util.Log;
 public class Mesh {
 	private final String TAG = "Mesh";
 	protected FloatBuffer vertexBuffer;
+	protected FloatBuffer textureBuffer;
 	protected ShortBuffer drawListBuffer;
 	protected int numVertices;
 	protected int coordsPerVertex = 3;
@@ -22,19 +23,27 @@ public class Mesh {
 	
 	public void draw(Shader shader){
 		int mPositionHandle;
+		int mTextureHandle;
 		int mColorHandle;
 		
 		//Log.i(TAG, "Rendering mesh...");
 		// Set up position array
-		mPositionHandle = shader.getAttribLocation("vPosition");
+		mPositionHandle = shader.getAttribLocation("aPosition");
 		GLES20.glEnableVertexAttribArray( mPositionHandle );
 		
 		GLES20.glVertexAttribPointer( mPositionHandle, coordsPerVertex,
 									  GLES20.GL_FLOAT, false,
 									  coordsPerVertex * 4, vertexBuffer );
 		
+		mTextureHandle = shader.getAttribLocation("aTexCoord");
+		GLES20.glEnableVertexAttribArray( mTextureHandle );
+		
+		GLES20.glVertexAttribPointer( mTextureHandle, 2,
+									  GLES20.GL_FLOAT, false,
+									  2 * 4, textureBuffer);
+		
 		final float color[] = {0.2f, 0.7f, 0.8f, 1.f};
-		mColorHandle = shader.getUniformLocation("vColor");
+		mColorHandle = shader.getUniformLocation("uColor");
 		GLES20.glUniform4fv(mColorHandle, 1, color, 0);
 		
 		// Draw the mesh.
@@ -61,6 +70,15 @@ public class Mesh {
 		vertexBuffer = bb.asFloatBuffer();
 		vertexBuffer.put( coords );
 		vertexBuffer.position( 0 );
+	}
+	
+	
+	protected void loadTextureBuffer( float coords[] ){
+		ByteBuffer bb = ByteBuffer.allocateDirect( coords.length * 4 );
+		bb.order( ByteOrder.nativeOrder() );
+		textureBuffer = bb.asFloatBuffer();
+		textureBuffer.put( coords );
+		textureBuffer.position( 0 );
 	}
 	
 	/**
--- src/github/MichaelBeeu/threadedtexture/vos/TestMesh.java
+++ src/github/MichaelBeeu/threadedtexture/vos/TestMesh.java
@@ -5,9 +5,14 @@ public class TestMesh extends Mesh {
 								   -0.5f, -0.5f, 0.f,
 								   0.5f, -0.5f, 0.f,
 								   0.5f, 0.5f, 0.f};
+	static final float texCoords[] = {0f, 1f,
+		                              0f, 0f,
+		                              1f, 0f,
+		                              1f, 1f};
 	private final short drawOrder[] = {0, 1, 2, 0, 2, 3};
 	
 	public TestMesh(){
+		loadTextureBuffer( texCoords );
 		loadVertexBuffer( squareCoords );
 		loadDrawBuffer( drawOrder );
 		setNumVertices( drawOrder.length );
